40fccc853760655ddecd19bf34885a5fa90086ec
DESKTOP-JRU1C5E\KSR <jak92226@gmail.com>
2021-11-01 10:28:18 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-//SEARCH FOCUS FUNC
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
-
 //BADGE,GO TO TOP FUNC
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -144,7 +126,3 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-//FOOTER THISYEAR
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
